a649513e3db57cd85abe85b56c799e564ed2c323
mehedihasanrupok <hasanmehedi4114@gmail.com>
2023-04-12 22:24:34 +0600

    ErrorFileDesign

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Error.css';
+import img from '../../../public/Icons/frame11.jpg';
 
 const Error = () => {
     return (
         <div className='error'>
-            <img src="../../../public/Icons/frame11.jpg" alt="" />
+            <img src={img} alt="" />
             <div className='Another'>
                 <h1>404 The Error</h1>
                 <h3 className='details'> What You are Searching Are not available Right Now</h3>
